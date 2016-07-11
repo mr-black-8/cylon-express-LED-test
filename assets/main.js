36bf04e6aa66b8f8c5742d9cc154fcdaae213d79
mr-black-8 <boz.black@gmail.com>
2016-07-11 16:31:14 +1000

    first led test

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,1 @@
-var Cylon = require('cylon');
-
-// Cylon.robot({
-//   connections: {
-//     raspi: { adaptor: 'raspi' }
-//   },
-//
-//   devices: {
-//     led: { drived: 'led' }
-//   },
-//
-//   work: function() {
-//     every((1).second(), function(){
-//       num = Math.random() * 255
-//       my.led.brightness(num)
-//     })
-//   }
-// }).start();
+console.log($)
--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 var express = require('express');
+var Cylon = require('cylon');
+
+var pi = Cylon.robot({
+  connections: {
+    raspi: { adaptor: 'raspi' }
+  },
+
+  devices: {
+    led: { driver: 'led' }
+  },
+
+  work: function() {
+    every((1).second(), function(){
+      num = Math.random() * 255
+      my.led.brightness(num)
+    })
+  }
+})
 
 var app = express();
 app.use( express.static("assets") );
@@ -7,6 +25,23 @@ app.listen(1337, function(){
   console.log("Server started on port 1337")
 });
 
+app.all(/.*/, function(req, res, next) {
+  console.log( req.method, "request for: ", req.url );
+  next();
+});
+
 app.get("/", function(req, res) {
   res.sendFile( __dirname + "/index.html");
+});
+
+app.get("/on", function(req, res) {
+  console.log("on")
+  pi.start();
+  res.redirect("/")
+});
+
+app.get("/off", function(req, res) {
+  console.log("off")
+  pi.halt();
+  res.redirect("/")
 });
